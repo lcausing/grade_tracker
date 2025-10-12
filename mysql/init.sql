1f07a8e5be9b51c27866310cd529c2c96996575b
lcausing <causingljbcit@gmail.com>
2025-10-11 21:31:03 -0700

    gave appuser full priveleges in order to update data. appuser can be used in the future for connection

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,7 +1,14 @@
 CREATE DATABASE IF NOT EXISTS grade_tracker;
-
 USE grade_tracker;
 
+-- ensure user exists (in case container rebuilds)
+CREATE USER IF NOT EXISTS 'appuser'@'%' IDENTIFIED BY 'apppass';
+
+-- grant full privileges on this DB
+GRANT ALL PRIVILEGES ON grade_tracker.* TO 'appuser'@'%';
+FLUSH PRIVILEGES;
+
+-- now create table + seed data
 CREATE TABLE IF NOT EXISTS students (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
