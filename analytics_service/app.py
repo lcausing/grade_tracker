f9650a3e2cc8d1326d510736c291b7957e7d973e
Louie Causing <lcausing@my.bcit.ca>
2025-10-15 11:28:14 -0700

    made app.py running on port 5003

---
+++ analytics_service/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from mysql_connector import get_grades
+from mongodb_connector import store_analytics
+from utils import compute_stats
+
+app = Flask(__name__)
+
+@app.route('/analytics', methods=['GET'])
+def analytics():
+    grades = get_grades()
+    stats = compute_stats(grades)
+    store_analytics(stats)
+    return jsonify(stats)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5003)
